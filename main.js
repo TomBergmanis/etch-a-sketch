271c97429c1562d83be177b5ff816d1f7723ac15
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2023-10-25 19:54:37 +0100

    added a working grid using flexbox and a loop method.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const grid = document.getElementById("grid");
+const grid = document.querySelector(".grid");
 
+// Use a nested for loop to create the grid cells
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const cell = document.createElement("div");

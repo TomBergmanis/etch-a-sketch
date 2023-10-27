faa0c21da804580b85ee69e621abfe8150d7ac63
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2023-10-27 15:43:24 +0100

    restarting the project, adding in the grid from before and the hover feature

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 const grid = document.querySelector(".grid");
-const apply = document.querySelector(".apply");
-const resetBtn = document.querySelector(".reset");
-
-defaultGrid = 16;
-let gridSize = 16;
-
-grid.addEventListener("mouseover", function (e) {
-  if (e.target.matches(".cell")) {
-    e.target.classList.add("active");
-  }
-});
-
-apply.addEventListener("click", GridSizeValue);
-
-function GridSizeValue() {
-  console.log("apply button clicked");
-  let gridSize = prompt("Choose the number of squares in your grid", "0");
-  console.log(gridSize);
-}
 
 // Use a nested for loop to create the grid cells
-
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const cell = document.createElement("div");
@@ -28,3 +8,9 @@ for (let i = 0; i < 16; i++) {
     grid.appendChild(cell);
   }
 }
+// hovers over the grid and changes the color / fills them in
+grid.addEventListener("mouseover", function (e) {
+  if (e.target.matches(".cell")) {
+    e.target.classList.add("active");
+  }
+});

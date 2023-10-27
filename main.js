bbbb6cb2cbd0f50eafe36f99e2c5c39774d76afd
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2023-10-27 13:07:55 +0100

    part 3 of the project complete, hovering and color changing added

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const grid = document.querySelector(".grid");
+const apply = document.querySelector(".apply");
+const resetBtn = document.querySelector(".reset");
+
+defaultGrid = 16;
+let gridSize = 16;
+
+grid.addEventListener("mouseover", function (e) {
+  if (e.target.matches(".cell")) {
+    e.target.classList.add("active");
+  }
+});
+
+apply.addEventListener("click", GridSizeValue);
+
+function GridSizeValue() {
+  console.log("apply button clicked");
+  let gridSize = prompt("Choose the number of squares in your grid", "0");
+  console.log(gridSize);
+}
 
 // Use a nested for loop to create the grid cells
+
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const cell = document.createElement("div");
